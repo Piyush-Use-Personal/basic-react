4e2e4a8828001a6c0468746dbbcce60a5132724e
Piyush Dubey <piyush115dubey@gmail.com>
2023-02-12 17:48:20 +0530

    react custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import usePerson from "./hooks/usePerson";
 
 function App() {
+  const { name, role, onChange } = usePerson();
+
+  const changeName = () => {
+    onChange('name', 'name changed')
+  }
+  const changeRole = () => {
+    onChange('role', 'role changed')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Name: {name}</p>
+        <p>Role: {role}</p>
+        <button onClick={changeName}>Change name</button>
+        <button onClick={changeRole}>Change role</button>
       </header>
     </div>
   );
